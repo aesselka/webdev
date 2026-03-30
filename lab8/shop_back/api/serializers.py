19e14625ce1998e7d0da3b9ac58502b641b2f96c
Assel Bazhikey <a_bazhikey@kbtu.kz>
2026-03-30 16:30:58 +0500

    lab9

--- lab8/shop_back/api/serializers.py
+++ lab8/shop_back/api/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category']
+        fields = '__all__'
+        # fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category']
